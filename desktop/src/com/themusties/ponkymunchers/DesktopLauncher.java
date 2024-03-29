4fe06f1f8e3696b2978e7c8266eb2c3452d75bf3
ambrosia13 <ivynow.13@gmail.com>
2024-03-28 18:24:43 -0700

    Change OpenGL version to 3.2

--- desktop/src/com/themusties/ponkymunchers/DesktopLauncher.java
+++ desktop/src/com/themusties/ponkymunchers/DesktopLauncher.java
@@ -10,6 +10,8 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("PonkyMunchers");
+		config.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.GL32, 3, 2);
+		
 		new Lwjgl3Application(new PonkyMunchersGame(), config);
 	}
 }
